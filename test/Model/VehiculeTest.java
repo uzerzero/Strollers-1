e2e5277d24ce290a67f4d31c93262f2ec09ecc2b
uzerzero <clodagh.veale@gmail.com>
2014-02-14 16:07:43 +0100

    Tests

---
+++ test/Model/VehiculeTest.java
@@ -0,0 +1,51 @@
+package model;
+
+/**
+ * Created by clodagh on 14/02/14.
+ */
+public class VehiculeTest {
+    @org.junit.Test
+    public void testGetPosition() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetPosition() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetSpeedKmPerHour() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetSpeedKmPerHour() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testMove() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testTurn() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSpeedUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testStop() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testToString() throws Exception {
+
+    }
+}
